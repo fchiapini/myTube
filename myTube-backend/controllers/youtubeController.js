b21b234dc4ae67f3ad863b0e59379a9f59dd613d
Felipe Chiapini <felipe.chiapini@gmail.com>
2020-04-27 12:13:57 +0900

    Group videos by subject from Youtube Playlist Api

--- myTube-backend/controllers/youtubeController.js
+++ myTube-backend/controllers/youtubeController.js
@@ -1,17 +1,38 @@
 import { youtubeService } from '../services/youtubeService.js'
-import topics from '../topics.json'
+import data from '../topics.json'
+
+const isPlaylistFromSubject = (playlist, subject) =>
+  subject.playlistIds.includes(playlist[0].snippet.playlistId)
+
+const groupVideosBySubject = (subjects, playlists) => {
+  let videosBySubject = []
+  subjects.forEach((subject) => {
+    let videos = [].concat.apply(
+      [],
+      playlists.filter((playlist) => isPlaylistFromSubject(playlist, subject))
+    )
+    videosBySubject.push({
+      subject: subject.title,
+      videos: videos
+    })
+  })
+  return videosBySubject
+}
 
 const getVideos = async (req, res) => {
   const { topic } = req.params
 
-  const playlistIds = PLAYLIST_IDS_BY_TOPIC[topic]
+  const subjects = data.topics.find((tpc) => tpc.name === topic)['subjects']
+
   let promises = []
-  playlistIds.forEach((playlistId) => {
-    promises.push(youtubeService.getVideosFromPlaylist(playlistId, '', []))
+  subjects.forEach((subject) => {
+    subject.playlistIds.map((playlistId) => {
+      promises.push(youtubeService.getVideosFromPlaylist(playlistId, '', []))
+    })
   })
-  Promise.all(promises).then((responses) => {
-    const videos = [].concat.apply([], responses)
-    res.json(videos)
+  Promise.all(promises).then((playlists) => {
+    const videosBySubject = groupVideosBySubject(subjects, playlists)
+    res.json(videosBySubject)
   })
 }
 
