a573668318b8bcd098e558c224688838d41300d8
Felipe Chiapini <felipe.chiapini@gmail.com>
2020-05-04 10:29:35 +0900

    Enable CORS

--- myTube-backend/functions/src/index.ts
+++ myTube-backend/functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import * as cors from 'cors'
 
 import { router } from './routes/routes.js'
 
@@ -11,6 +12,11 @@ require('dotenv').config()
 
 const app = express()
 
+const corsOptions = {
+  origin: process.env.ORIGIN_CORS
+}
+
+app.use(cors(corsOptions))
 app.use('/api', router)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
